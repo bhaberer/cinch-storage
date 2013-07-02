5c0e5d55345d173dcc1178f33c671b2b4d7eeadf
Brian Haberer <bhaberer@gmail.com>
2013-07-02 00:41:51 -0700

    Fixing conflict

--- lib/cinch-storage.rb
+++ lib/cinch-storage.rb
@@ -1,1 +1,25 @@
-require 'cinch/storage'
+require 'cinch/storage/version'
+require 'psych'
+
+class CinchStorage
+  attr_accessor :filename, :data
+
+  def initialize(file, init = Hash.new)
+    @filename = file
+    @data = YAML::load(File.open(@filename)) if File::exist?(@filename)
+    @data ||= init
+  end
+
+  def save
+    File.open(@filename, 'w') do |file|
+      YAML::dump(@data, file)
+    end
+  end
+
+  def synced_save(bot = @bot)
+    bot.synchronize(@filename) do
+      save
+    end
+  end
+end
+
